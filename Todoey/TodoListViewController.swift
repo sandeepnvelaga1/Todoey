1fb7b057b988b9893f472860cc1ff8a6925cb353
sai <saiios2358@gmail.com>
2018-03-30 17:16:02 -0400

    Data persisted using user defaults method.

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,89 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Sandeep Velaga on 3/26/18.
+//  Copyright © 2018 Sandeep. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController{
+
+    var itemArray = ["Find Someone","Talk","Kill"]
+    
+    let defaults = UserDefaults.standard
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        if let items = defaults.array(forKey: "TodoListArray") as? [String]{
+            itemArray = items
+        }
+            
+        
+        
+        
+    }
+// MARK - TableView Datasource Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    //MARK - TableView Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark
+        {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        else
+        {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        
+    }
+    
+//MARK -Add New Items
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem){
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle:.alert)
+        
+        let action = UIAlertAction(title: "Add Item", style:.default) { (action) in
+            //what will happen once user clicks the Add button Item on our UIAlert
+            self.itemArray.append(textField.text!)
+            
+            self.defaults.set(self.itemArray, forKey:"TodoListArrary")
+            
+            self.tableView.reloadData()
+            
+            }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    
+}
+
